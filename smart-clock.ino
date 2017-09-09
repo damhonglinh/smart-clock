1405df958268e9b942a158d4dee79ca63c4b10a1
Linh Dam <damhonglinh@gmail.com>
2017-09-09 17:56:04 +0700

    Refactor

--- smart-clock.ino
+++ smart-clock.ino
@@ -1,95 +1,112 @@
 #include <ESP8266WiFi.h>
-int redPin = 15;
-int greenPin = 12;
-int bluePin = 13;
-int LDRPin = A0;
-const char* ssid = "Microsoft";
-const char* password = "tivitulanh";
+#define RED_PIN 15
+#define GREEN_PIN 12
+#define BLUE_PIN 13
+#define LDR_PIN A0
+#define HTTP_PORT 443
+#define TIME_OUT 30000
 
+const char* ssid = "Microsoft";
+const char* password = "password";
 const char* host1 = "judgelinh.ngrok.io";
-boolean onOff = true;
+bool onOff = true;
 
 void setup() {
   Serial.begin(115200);
-  pinMode(redPin, OUTPUT);
-  pinMode(bluePin, OUTPUT);
-  pinMode(greenPin, OUTPUT);
+  pinMode(RED_PIN, OUTPUT);
+  pinMode(BLUE_PIN, OUTPUT);
+  pinMode(GREEN_PIN, OUTPUT);
   connectWifi();
 }
 
 void loop() {
   Serial.println("Looping...");
   ledGreen();
-  int LDRReading = 0;
-  LDRReading = analogRead(LDRPin);
   yield();
-  ReadFromURL(host1, 443, "/");
+  readFromURL(host1, "/");
   delay(10000);
 }
 
-void ReadFromURL(const char* host, int httpPort, String url) {
+void readFromURL(const char* host, String url) {
   Serial.print("connecting to ");
   Serial.println(host);
 
   // Use WiFiClient class to create TCP connections
   WiFiClientSecure client;
 
-  if (!client.connect(host, httpPort)) {
-    Serial.println("connection failed");
-    ledRed();
-    yield();
+  if (!connectTo(client, host)) {
     return;
   }
 
-  // We now create a URI for the request
-  Serial.print("Requesting URL: ");
-  Serial.println(url);
+  Serial.println("Requesting URL: " + url);
 
   // This will send the request to the server
   client.print(String("GET ") + url + " HTTP/1.1\r\n" +
                "Host: " + host + "\r\n" +
                "Connection: close\r\n\r\n");
-  unsigned long timeout = millis();
-  while (client.available() == 0) {
-    if (millis() - timeout > 5000) {
-      Serial.println(">>> Client Timeout !");
-      client.stop();
-      return;
-    }
+
+  if (!waitForConnection(client)) {
+    return;
   }
 
   // Read all the lines of the reply from server and print them to Serial
   while (client.available()) {
     String tempMsg = client.readStringUntil('\n');
-    if (tempMsg.length() == 1) { //ignores the headers
-      if (tempMsg == "0") {
-        onOff = false;
-        Serial.println(onOff);
-      }
-      if (tempMsg == "1") {
-        onOff = true;
-        Serial.println(onOff);
-      }
-    }
-
     Serial.println(tempMsg);
   }
 
-  Serial.println();
-  Serial.println("closing connection");
+  Serial.println("============== Closing connection");
+}
+
+
+bool connectTo(WiFiClientSecure& client, const char* host) {
+  if (!client.connect(host, HTTP_PORT)) {
+    Serial.println("Connection FAILED");
+    ledRed();
+    yield();
+    return false;
+  }
+  return true;
+}
+
+bool waitForConnection(WiFiClientSecure& client) {
+  unsigned long timeout = millis();
+  while (client.available() == 0) {
+    yield();
+
+    if (millis() - timeout > TIME_OUT) {
+      Serial.println(">>> Client Timeout !");
+      client.stop();
+      ledColor(255, 160, 176); // pink
+      return false;
+    }
+  }
+  return true;
 }
 
 void ledColor(int red, int green, int blue) {
-  if (onOff == true) {
-    analogWrite(redPin, red);
-    analogWrite(greenPin, green);
-    analogWrite(bluePin, blue);
+  if (onOff) {
+    analogWrite(RED_PIN, red);
+    analogWrite(GREEN_PIN, green);
+    analogWrite(BLUE_PIN, blue);
+  } else {
+    ledOff();
   }
 }
+
+void ledOff() {
+  analogWrite(RED_PIN, 0);
+  analogWrite(GREEN_PIN, 0);
+  analogWrite(BLUE_PIN, 0);
+}
+
 void ledRed()   { ledColor(255, 0, 0); }
 void ledGreen() { ledColor(0, 255, 0); }
 void ledBlue()  { ledColor(0, 0, 255); }
 
+int readLDR() {
+  return analogRead(LDR_PIN);
+}
 
 
 void connectWifi() {
