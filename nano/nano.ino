2023d974e63595cf0a4cc43d209066f5b6da9db9
Linh Dam <damhonglinh@gmail.com>
2017-09-15 01:12:52 +0700

    Add codes to read temperature

--- nano/nano.ino
+++ nano/nano.ino
@@ -2,10 +2,13 @@
 #include <Wire.h>
 #include <LCD.h>
 #include <LiquidCrystal_I2C.h>
+#include <OneWire.h>
+#include <DallasTemperature.h>
 
 #define LCD_LEN 16
 #define CHAR_COUNT_TO_SLIDE_PER_LCD_BLINK 4
 #define LCD_BLINK_INTERVAL 600
+#define ONE_WIRE_BUS 7
 
 SoftwareSerial ESPserial(2, 3); // RX | TX
 LiquidCrystal_I2C lcd(0x27, 2, 1, 0, 4, 5, 6, 7); // 0x27 is the I2C bus address for an unmodified backpack
@@ -13,6 +16,11 @@ String wifiInput;
 String nextWifiInput;
 unsigned long prevMillisLcd = 0;
 int lcdIndexFrom = 0;
+OneWire oneWire(ONE_WIRE_BUS);
+DallasTemperature sensors(&oneWire); // Pass our oneWire reference to Dallas Temperature.
+
+
+// ========== setup ==========
 
 void setup() {
   Serial.begin(9600);     // communication with the host computer
@@ -21,6 +29,8 @@ void setup() {
   lcd.begin(16, 2); // for 16 x 2 LCD module
   lcd.setBacklightPin(3, POSITIVE);
   lcd.setBacklight(HIGH);
+
+  sensors.begin();
 
   Serial.println("Ready\n");
 }
@@ -34,6 +44,10 @@ void loop() {
   }
 
   processLcdThread(currentMillis);
+
+  Serial.print("Temperature is: ");
+  Serial.println(getTemperature());
+  delay(1000);
 
   // delay(1000);
 }
@@ -81,4 +95,11 @@ void fillCharactersToArray(char* arr, int n, char character) {
     arr[i] = character;
   }
   arr[n] = 0;
+}
+
+// ========== Temperature ==========
+
+float getTemperature() {
+  sensors.requestTemperatures();
+  return sensors.getTempCByIndex(0);
 }
